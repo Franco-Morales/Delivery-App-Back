560fe0e6b096096fd663865f9bdb672da6bb5ed1
Franco <francomorales145@gmail.com>
2021-04-07 23:30:31 -0300

    Conexión  mongoDB lista

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ server.use(Express.urlencoded({extended:false}));
 
 //Settings
 server.set('port', process.env.PORT || 3000);
-console.log(process.env.PORT );
 
 //Routes
 server.get('/',(req,res)=>res.send("Bienvenido a Delivery App Backend [API_REST]"));
